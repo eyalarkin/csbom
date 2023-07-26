c83b7167446fa34b16e03886917c63f8d94a987e
Eyal Arkin <eyal@arkinnet.com>
2023-07-26 15:16:18 +0300

    Adding functionality

--- build/lib/csbom.py
+++ build/lib/csbom.py
@@ -2,8 +2,23 @@ import click
 
 from app import application
 
-@click.command()
-@click.option('-s', '--sbom', required=True, help='Path to the SBOM file')
+@click.group
+def cli():
+    pass
+
+@cli.command()
+@click.argument('sbom')
 @click.option('-o', '--output', default='bom-analysis.csv', show_default=True, help='Intended name for output file')
-def cli(sbom, output):
+def file2table(sbom, output):
+    """Given an SBOM, output a table of important info"""
     application.parse_sbom(sbom, output)
+
+@cli.command()
+@click.argument('sbom')
+@click.option('-o', '--output', default='dep-analysis.csv', show_default=True, help='Intended name for output file')
+def dep2table(sbom, output):
+    """Given an SBOM, output a table of dependencies"""
+    if application.dependencies(sbom):
+        application.parse_dependencies(sbom, output)
+    else:
+        click.echo('ERROR: This SBOM file does not contain any dependencies. Please ensure the SBOM has a dependencies array as one of the initial keys in the JSON object.')
